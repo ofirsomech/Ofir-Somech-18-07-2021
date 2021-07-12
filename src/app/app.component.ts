48094d404a1348a9f3bedead2dddcef7a5084e9b
ofirs <ofirs@codeoasis.com>
2021-07-12 22:08:28 +0300

    add dark/light mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Mode } from './models/mode.enum';
 import * as actions from "./store/weather.actions";
 
 @Component({
@@ -14,6 +15,21 @@ export class AppComponent implements OnInit {
   constructor(private store:Store){
   }
   title = 'weather';
+
+  
+  mode = Mode.Light;
+
+  switchMode(mode: Mode) {
+    if (mode === Mode.Light) {
+      document.querySelector('body')?.style.setProperty('--bg-color', '#272727');
+      document.querySelector('body')?.style.setProperty('--text-color', '#f8fafb');
+      this.mode = Mode.Dark;
+    } else {
+      document.querySelector('body')?.style.setProperty('--bg-color', '#f8fafb');
+      document.querySelector('body')?.style.setProperty('--text-color', '#272727');
+      this.mode = Mode.Light;
+    }
+  }
 
   
 }
