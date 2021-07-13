7d45ed923411c46d1f8f69d7b46bb64b5ef0fb50
ofirs <ofirs@codeoasis.com>
2021-07-13 18:23:21 +0300

    init dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: false,
+  weatherAPIKey: 'nTTVvhw0Gua7ql4Dz6KSAfV5uXJEhAFR',
+  apiDaily: 'https://dataservice.accuweather.com/currentconditions/v1',
+  apiForecast: 'https://dataservice.accuweather.com/forecasts/v1/daily/5day',
+  apiAutocomplete: 'https://dataservice.accuweather.com/locations/v1/cities/autocomplete',
+  apiGeoLocation: 'https://dataservice.accuweather.com/locations/v1/cities/geoposition/search',
 };
