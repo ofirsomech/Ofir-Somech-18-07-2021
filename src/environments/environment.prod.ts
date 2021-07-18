8177e774cf87510c910daa4555d8c6feaeae3d62
ofirs <ofirs@codeoasis.com>
2021-07-18 17:22:43 +0300

    wip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  weatherAPIKey: 'K501cfyfAaQVF0AgJOGOl7zCm4WC1g5Z',
+  weatherAPIKey: 'dWhFC7KpBi5sZZZeXA8RArxySuVi7dYm',
   apiDaily: 'https://dataservice.accuweather.com/currentconditions/v1',
   apiForecast: 'https://dataservice.accuweather.com/forecasts/v1/daily/5day',
   apiAutocomplete: 'https://dataservice.accuweather.com/locations/v1/cities/autocomplete',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  weatherAPIKey: 'K501cfyfAaQVF0AgJOGOl7zCm4WC1g5Z',
+  weatherAPIKey: 'dWhFC7KpBi5sZZZeXA8RArxySuVi7dYm',
   apiDaily: 'https://dataservice.accuweather.com/currentconditions/v1',
   apiForecast: 'https://dataservice.accuweather.com/forecasts/v1/daily/5day',
   apiAutocomplete: 'https://dataservice.accuweather.com/locations/v1/cities/autocomplete',
